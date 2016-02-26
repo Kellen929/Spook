43a12be2b09c9a508356bfca1c267c4876d12030
Tim Mendez <hikeemhigh@gmail.com>
2016-02-25 20:05:46 -0800

    shooting

---
+++ Assets/Scripts/Player/Shooting.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour {
+	public GameObject bulletPrefab;
+	public Camera cam;
+
+	private const float bulletImpulse = 35.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")) {
+			GameObject bullet = (GameObject)Instantiate(bulletPrefab, cam.transform.position + cam.transform.forward, cam.transform.rotation * bulletPrefab.transform.rotation);
+			bullet.GetComponent<Rigidbody>().AddForce(cam.transform.forward * bulletImpulse, ForceMode.Impulse);
+		}
+	}
+}
