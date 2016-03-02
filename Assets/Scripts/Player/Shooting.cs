af06e03c638e8c254bd185fa1ce125e4f43a7e1d
Tim Mendez <hikeemhigh@gmail.com>
2016-03-02 11:33:42 -0800

    sound effects for player movement + struggle

--- Assets/Scripts/Player/Shooting.cs
+++ Assets/Scripts/Player/Shooting.cs
@@ -4,8 +4,12 @@ using System.Collections;
 public class Shooting : MonoBehaviour {
 	public GameObject bulletPrefab;
 	public Camera cam;
+	public AudioSource sfx;
+	public AudioClip gunshot;
+	public AudioClip shellFall;
 
 	private const float bulletImpulse = 35.0f;
+	private const float SHELL_DELAY = 0.2f;
 
 	// Use this for initialization
 	void Start () {
@@ -15,8 +19,15 @@ public class Shooting : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetButtonDown("Fire1")) {
+			sfx.PlayOneShot(gunshot);
+			StartCoroutine(PlayShellSFX(SHELL_DELAY));
 			GameObject bullet = (GameObject)Instantiate(bulletPrefab, cam.transform.position + cam.transform.forward, cam.transform.rotation * bulletPrefab.transform.rotation);
 			bullet.GetComponent<Rigidbody>().AddForce(cam.transform.forward * bulletImpulse, ForceMode.Impulse);
 		}
+	}
+
+	IEnumerator PlayShellSFX(float waitTime) {
+		yield return new WaitForSeconds(waitTime);
+		sfx.PlayOneShot(shellFall);
 	}
 }
