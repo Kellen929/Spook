14313ea71b41d840a9e261f5f589b5cb6c4a2964
Lucas Salisbury <lucassalisbury@gmail.com>
2016-03-05 15:33:42 -0800

    added door and open/close functionality

---
+++ Assets/Scripts/Environment/DoorOpenClose.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpenClose : MonoBehaviour {
+	public Transform doorTransform;
+	private bool doorOpen = false, opening = false;
+	private int count = 0;
+	public Transform playerTransform;
+	//private int xDistAway = 0, zDistAway = 0;
+	private int dist = 4;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// door.position.x - player.position.x 
+		// take abs val of that
+		// distance = 10
+		// if calcDistance <= 10 set closeEnough to true or false;
+
+		// input key == 'e' && position is close enough to the door
+
+		//print ("door position = " + doorTransform.localPosition.x);
+		//print ("playerPosition = " + playerTransform.localPosition.x);
+
+//		print ("door y position = " + doorTransform.localPosition.z);
+//		print ("playerPosition = " + playerTransform.localPosition.z);
+		bool playerCloseEnough = closeEnough();
+
+		if (Input.GetKeyDown(KeyCode.E) && !doorOpen && playerCloseEnough)  {
+			opening = true;
+		}
+		else if (Input.GetKeyDown(KeyCode.E) && doorOpen && playerCloseEnough) {
+			//doorTransform.Rotate (new Vector3 (0, -(doorTransform.rotation.y - 0.5f), 0));	
+			opening = false;
+		}
+
+		if (opening && !doorOpen) {
+			doorTransform.Rotate (new Vector3 (0, (doorTransform.rotation.y - 0.5f), 0)); 
+			//print ("door rotation = " + doorTransform.localRotation.y);
+
+			if (doorTransform.localRotation.y >= 0.98f) {
+				doorOpen = true;
+			}
+		} else if (!opening && doorOpen) {
+			doorTransform.Rotate (new Vector3 (0, -(doorTransform.rotation.y - 0.5f), 0)); 
+
+			if (doorTransform.localRotation.y <= 0.72f) {
+				doorOpen = false;
+			}
+		}
+	}
+
+	private bool closeEnough() {
+		float playerx = playerTransform.localPosition.x;
+		float playerz = playerTransform.localPosition.z;
+		float doorx = doorTransform.localPosition.x;
+		float doorz = doorTransform.localPosition.z;
+
+		double xDistAway = Mathf.Abs (doorx - playerx);
+		double zDistAway = Mathf.Abs (doorz - playerz);
+
+		if (xDistAway <= dist && zDistAway <= dist) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
