e02ee43ace8990e38224a6ecf90472fc20549aa7
Lucas Salisbury <lucassalisbury@gmail.com>
2016-03-06 11:46:43 -0800

    added start menu

---
+++ Assets/Standard Assets/Utility/PlayGameButtonClick.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayGameButtonClick : MonoBehaviour {
+	AsyncOperation levelAsync;
+
+	// Use this for initialization
+	void Start () {
+		levelAsync = Application.LoadLevelAdditiveAsync("leve1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadGame() {
+		Application.LoadLevel("level1");
+	}
+}
