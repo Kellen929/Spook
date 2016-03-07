228969815d3dc7ef0265550989c052bc8ae9f4fb
Classroom Tech User <classroomtech@pcp137993pcs.wireless.calpoly.edu>
2016-03-06 20:06:26 -0800

    save

---
+++ Assets/Scripts/Spider/BulletInteraction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletInteraction : MonoBehaviour {
+	public int health = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void decreaseHealth(int damage) {
+		health -= damage;
+
+		if(health <= 0)
+			killSelf();
+	}
+
+	private void killSelf() {
+		Destroy(gameObject);
+	}
+}
