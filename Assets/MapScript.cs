9ce721ad24d641b08da22ef2c779fcd078b69a61
dualer208 <dualer208@yahoo.com>
2016-03-06 17:18:52 -0800

    git message

--- Assets/MapScript.cs
+++ Assets/MapScript.cs
@@ -4,62 +4,60 @@ using System.Collections;
 public class MapScript : MonoBehaviour {
 
 	public bool map = false;
+	public bool toggle = false;
 	public float wait = 0.25f;
-	public float moveSpeed = 0.01f;
+	public float moveSpeed = 0.1f;
+	Vector3 TargetPosition = new Vector3 (0, -0.5f, 0.5f);
 
 	void Start () {
-		gameObject.GetComponent<Renderer>().enabled = false;
+		
+		gameObject.GetComponent<Renderer>().enabled = true;
 	}
 
 
 	void Update () {
 
 		if (Input.GetKeyDown(KeyCode.M)) {
+			toggle = true;
+		}
 
-			//MoveTowardsTarget ();
-			StartCoroutine(open());
+		if (toggle) {
+			
+			open ();
 		}
 	}
 
-	IEnumerator open() {
+
+
+	void open() {
 
 		// yes map -> put away map
 		if (map) {
+			
+			float step = moveSpeed * Time.deltaTime;
+			transform.position = Vector3.Translate (transform.position, TargetPosition, step);
 
-			yield return new WaitForSeconds(wait);
-			gameObject.GetComponent<Renderer> ().enabled = false;
-			map = false;
+			if(transform.position == TargetPosition){
+				gameObject.GetComponent<Renderer> ().enabled = false;
+				map = false;
+				toggle = false;
+			}
 
 		} 
 		// no map -> take out map
 		else if (!map) {
 
-			yield return new WaitForSeconds(wait);
-			gameObject.GetComponent<Renderer> ().enabled = true;
-			map = true;
+			float step = moveSpeed * Time.deltaTime;
+			transform.position = Vector3.Translate (transform.position, -TargetPosition, step);
+
+
+			if (transform.position == TargetPosition) {
+				gameObject.GetComponent<Renderer> ().enabled = true;
+				map = true;
+				toggle = false;
+			}
 		}
 	}
 
-	//move towards a target at a set speed.
-	private void MoveTowardsTarget() {
-		//the speed, in units per second, we want to move towards the target
-		float speed = 1;
-		//move towards the center of the world (or where ever you like)
-		Vector3 targetPosition = new Vector3(0, 0.65F, 0);
 
-		Vector3 currentPosition = this.transform.position;
-		//first, check to see if we're close enough to the target
-		if(Vector3.Distance(currentPosition, targetPosition) > .01f) { 
-			Vector3 directionOfTravel = targetPosition - currentPosition;
-			//now normalize the direction, since we only want the direction information
-			directionOfTravel.Normalize();
-			//scale the movement on each axis by the directionOfTravel vector components
-
-			this.transform.Translate(
-				(directionOfTravel.x * speed * Time.deltaTime),
-				(directionOfTravel.y * speed * Time.deltaTime),
-				(directionOfTravel.z * speed * Time.deltaTime),
-				Space.World);
-		}
-	}
 }
