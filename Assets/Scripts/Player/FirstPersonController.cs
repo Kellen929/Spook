32f29bd31dfd7963e3b55a6308fa197b872d5913
Lucas Salisbury <lucassalisbury@gmail.com>
2016-03-07 15:16:21 -0800

    added escape button menu

--- Assets/Scripts/Player/FirstPersonController.cs
+++ Assets/Scripts/Player/FirstPersonController.cs
@@ -27,6 +27,7 @@ public class FirstPersonController : MonoBehaviour {
 	private Vector3 gunbobAxis;
 	private int gunbobCount = 0;
 	private bool isBobbingDown = true;
+	private bool isPaused = false;
 
 	// Private Constants
 	private const float MOVE_FASTER_MULTIPLIER = 6.0f;
@@ -50,103 +51,109 @@ public class FirstPersonController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// Rotation
-		transform.Rotate(0, Input.GetAxis("Mouse X") * MOUSE_SENSITIVITY, 0);
+		if (!isPaused) {
+			// Rotation
+			transform.Rotate(0, Input.GetAxis("Mouse X") * MOUSE_SENSITIVITY, 0);
 
-		verticalRotation -= Input.GetAxis("Mouse Y") * MOUSE_SENSITIVITY;
-		verticalRotation = Mathf.Clamp(verticalRotation, -PITCH_RANGE, PITCH_RANGE);
-		mainCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
-		spotLight.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+			verticalRotation -= Input.GetAxis("Mouse Y") * MOUSE_SENSITIVITY;
+			verticalRotation = Mathf.Clamp(verticalRotation, -PITCH_RANGE, PITCH_RANGE);
+			mainCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+			spotLight.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
 
-		
-		// Jumping
-		if (playerController.isGrounded) {
-			if(isFalling) {
-				sfx.PlayOneShot(landSFX);
-				isFalling = false;
-			}
-			if (Input.GetButton("Jump")) {
-				sfx.PlayOneShot(jumpSFX);
-				jumpVector.y = JUMP_FORCE;
-				isFalling = true;
-			}
-		}
-
-		jumpVector.y -= GRAVITY * Time.deltaTime;
-		playerController.Move(jumpVector * Time.deltaTime);
-
-
-		// Movement	
-		float forwardSpeed = Input.GetAxis("Vertical");
-		float sideSpeed = Input.GetAxis("Horizontal");
-
-		Vector3 velocity = new Vector3(sideSpeed * MOVE_FASTER_MULTIPLIER, 0, forwardSpeed * MOVE_FASTER_MULTIPLIER);
-		velocity = transform.rotation * velocity;
-		playerController.SimpleMove(velocity);
-
-		// Movement SFX & Gunbob
-		if(velocity != Vector3.zero) {
-			// Movement SFX
-			if(!sfx.isPlaying) {
-				switch(stepIdx) {
-					case 0:
-						sfx.PlayOneShot(step1SFX);
-						break;
-					case 1:
-						sfx.PlayOneShot(step2SFX);
-						break;
-					case 2:
-						sfx.PlayOneShot(step3SFX);
-						break;
-					case 3:
-						sfx.PlayOneShot(step4SFX);
-						break;
+			
+			// Jumping
+			if (playerController.isGrounded) {
+				if(isFalling) {
+					sfx.PlayOneShot(landSFX);
+					isFalling = false;
 				}
-				stepIdx = stepIdx >= 3 ? 0 : stepIdx + 1; 
+				if (Input.GetButton("Jump")) {
+					sfx.PlayOneShot(jumpSFX);
+					jumpVector.y = JUMP_FORCE;
+					isFalling = true;
+				}
 			}
-	
-			// Gunbob
-			if(isBobbingDown && gunbobCount <= 8) {
-				gunbobPos += gunTrans.up * Time.deltaTime * GUNBOB_SPEED;
 
-				if(gunbobCount++ >= 8)
-					isBobbingDown = false;
+			jumpVector.y -= GRAVITY * Time.deltaTime;
+			playerController.Move(jumpVector * Time.deltaTime);
+
+
+			// Movement	
+			float forwardSpeed = Input.GetAxis("Vertical");
+			float sideSpeed = Input.GetAxis("Horizontal");
+
+			Vector3 velocity = new Vector3(sideSpeed * MOVE_FASTER_MULTIPLIER, 0, forwardSpeed * MOVE_FASTER_MULTIPLIER);
+			velocity = transform.rotation * velocity;
+			playerController.SimpleMove(velocity);
+
+			// Movement SFX & Gunbob
+			if(velocity != Vector3.zero) {
+				// Movement SFX
+				if(!sfx.isPlaying) {
+					switch(stepIdx) {
+						case 0:
+							sfx.PlayOneShot(step1SFX);
+							break;
+						case 1:
+							sfx.PlayOneShot(step2SFX);
+							break;
+						case 2:
+							sfx.PlayOneShot(step3SFX);
+							break;
+						case 3:
+							sfx.PlayOneShot(step4SFX);
+							break;
+					}
+					stepIdx = stepIdx >= 3 ? 0 : stepIdx + 1; 
+				}
+		
+				// Gunbob
+				if(isBobbingDown && gunbobCount <= 8) {
+					gunbobPos += gunTrans.up * Time.deltaTime * GUNBOB_SPEED;
+
+					if(gunbobCount++ >= 8)
+						isBobbingDown = false;
+				}
+				else {
+					gunbobPos += (-1 * gunTrans.up) * Time.deltaTime * GUNBOB_SPEED;
+
+					if(gunbobCount-- <= 0)
+						isBobbingDown = true;
+				}
+				gunTrans.localPosition = gunbobPos + gunbobAxis * Mathf.Sin(Time.time * GUNBOB_FREQ) * GUNBOB_MAG;
 			}
-			else {
-				gunbobPos += (-1 * gunTrans.up) * Time.deltaTime * GUNBOB_SPEED;
 
-				if(gunbobCount-- <= 0)
-					isBobbingDown = true;
+			// Crouching
+			if (playerController.isGrounded && Input.GetKeyDown(KeyCode.C)) {
+				isCurrentlyCrouching = !isCurrentlyCrouching;
+				crouchState = true;
 			}
-			gunTrans.localPosition = gunbobPos + gunbobAxis * Mathf.Sin(Time.time * GUNBOB_FREQ) * GUNBOB_MAG;
-		}
 
-		// Crouching
-		if (playerController.isGrounded && Input.GetKeyDown(KeyCode.C)) {
-			isCurrentlyCrouching = !isCurrentlyCrouching;
-			crouchState = true;
-		}
+			if (crouchState && isCurrentlyCrouching && mainCamera.transform.position.y > MIN_CAMERA) {
+				Vector3 tmpCamPos = new Vector3 (mainCamera.transform.position.x, mainCamera.transform.position.y - CROUCH_SPEED, mainCamera.transform.position.z);
+				mainCamera.transform.position = tmpCamPos;
+				Vector3 tmpLightPos = new Vector3 (spotLight.transform.position.x, spotLight.transform.position.y - CROUCH_SPEED, spotLight.transform.position.z);
+				spotLight.transform.position = tmpLightPos;
 
-		if (crouchState && isCurrentlyCrouching && mainCamera.transform.position.y > MIN_CAMERA) {
-			Vector3 tmpCamPos = new Vector3 (mainCamera.transform.position.x, mainCamera.transform.position.y - CROUCH_SPEED, mainCamera.transform.position.z);
-			mainCamera.transform.position = tmpCamPos;
-			Vector3 tmpLightPos = new Vector3 (spotLight.transform.position.x, spotLight.transform.position.y - CROUCH_SPEED, spotLight.transform.position.z);
-			spotLight.transform.position = tmpLightPos;
+				// Crouching means quieter movement
+				sfx.volume = 0.2f;
+			}
+			else if (crouchState && !isCurrentlyCrouching && mainCamera.transform.position.y < MAX_CAMERA) {
+				Vector3 tmpCamPos = new Vector3 (mainCamera.transform.position.x, mainCamera.transform.position.y + CROUCH_SPEED, mainCamera.transform.position.z);
+				mainCamera.transform.position = tmpCamPos;
+				Vector3 tmpLightPos = new Vector3 (spotLight.transform.position.x, spotLight.transform.position.y + CROUCH_SPEED, spotLight.transform.position.z);
+				spotLight.transform.position = tmpLightPos;
 
-			// Crouching means quieter movement
-			sfx.volume = 0.2f;
+				// Standing means louder movement
+				sfx.volume = 0.5f;
+			}
+			else if (mainCamera.transform.position.y <= MIN_CAMERA || mainCamera.transform.position.y >= MAX_CAMERA) {
+				crouchState = false;
+			}
 		}
-		else if (crouchState && !isCurrentlyCrouching && mainCamera.transform.position.y < MAX_CAMERA) {
-			Vector3 tmpCamPos = new Vector3 (mainCamera.transform.position.x, mainCamera.transform.position.y + CROUCH_SPEED, mainCamera.transform.position.z);
-			mainCamera.transform.position = tmpCamPos;
-			Vector3 tmpLightPos = new Vector3 (spotLight.transform.position.x, spotLight.transform.position.y + CROUCH_SPEED, spotLight.transform.position.z);
-			spotLight.transform.position = tmpLightPos;
+	}
 
-			// Standing means louder movement
-			sfx.volume = 0.5f;
-		}
-		else if (mainCamera.transform.position.y <= MIN_CAMERA || mainCamera.transform.position.y >= MAX_CAMERA) {
-			crouchState = false;
-		}
+	public void togglePaused() {
+		isPaused = !isPaused;
 	}
 }
