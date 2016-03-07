205d1b60d3e53d612afa76abd96d7624729ac117
dualer208 <dualer208@yahoo.com>
2016-03-06 19:41:00 -0800

    advanced map

--- Assets/MapScript.cs
+++ Assets/MapScript.cs
@@ -3,12 +3,11 @@ using System.Collections;
 
 public class MapScript : MonoBehaviour {
 
-	public bool map = false;
-	private const float MOVE_SPEED = 0.1f;
 	public bool inProgress = false;
 	public bool showingMap = false;
 	private Vector3 FINAL_MAP_LOCATION = new Vector3(0, 0, 0.54F);
 	private Vector3 START_MAP_LOCATION = new Vector3(0, -.53F, 0.54F);
+	private const float MOVE_SPEED = 0.09f;
 	public Transform mapTrans;
 
 	//camera controlls
@@ -16,21 +15,26 @@ public class MapScript : MonoBehaviour {
 	public Camera mapCam;
 	public Camera mainCam;
 	public Light tabLight;
+	public Light pLight;
 
-
+	//start off map inactive
 	void Start () {
 		gameObject.GetComponent<Renderer>().enabled = false;
 		mapCam.enabled = false;
+		tabLight.enabled = false;
+		pLight.enabled = false;
 	}
 
 
 	void Update () {
 
+		//based on button press
 		if (Input.GetKeyDown(KeyCode.M) && !inProgress) {
 
 			showingMap = !showingMap;
 			inProgress = true;
 		}
+
 		if (showingMap && inProgress) {
 
 			gameObject.GetComponent<Renderer>().enabled = true;
@@ -40,15 +44,17 @@ public class MapScript : MonoBehaviour {
 			//map up
 			if (mapTrans.localPosition.y >= FINAL_MAP_LOCATION.y) {
 				mainCam.farClipPlane = 1;
-				mapLight.range = 100;
+				mapLight.range = 68;
 				inProgress = false;
 				mapCam.enabled = true;
+				pLight.enabled = true;
 			}
 
 		} else if (!showingMap && inProgress) {
 			
 			mapTrans.localPosition = Vector3.MoveTowards (mapTrans.localPosition, START_MAP_LOCATION, MOVE_SPEED);
 			mapCam.enabled = false;
+			pLight.enabled = false;
 			mapLight.range = 0;
 			mainCam.farClipPlane = 500;
 
@@ -60,6 +66,5 @@ public class MapScript : MonoBehaviour {
 				inProgress = false;
 			}
 		}
-
 	}
 }
