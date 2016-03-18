da04bee627dcd4e3429b2e1a13c9b6c45e63ded0
Tim Mendez <hikeemhigh@gmail.com>
2016-03-18 02:17:23 -0700

    more frequent ammo - once every 12 seconds

--- Assets/Scripts/Environment/AmmoSpawner.cs
+++ Assets/Scripts/Environment/AmmoSpawner.cs
@@ -9,34 +9,34 @@ public class AmmoSpawner : MonoBehaviour {
 	// Private variables
 	private bool ammoBoxOnMap = false;
 	private GameObject box;
+	private float spawnCountdown;
 
 	// Constants
-	private const float AMMO_SPAWN_DELAY = 10.0f;
+	private const float AMMO_SPAWN_DELAY = 12.0f;
 
 
 	// Use this for initialization
 	void Start () {
-	
+		spawnCountdown = AMMO_SPAWN_DELAY;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(!ammoBoxOnMap) {
-			StartCoroutine("spawnAmmoBox", AMMO_SPAWN_DELAY);
-			ammoBoxOnMap = true;
+		spawnCountdown -= Time.deltaTime;
+
+		if(spawnCountdown <= 0) {
+			spawnAmmoBox();
+			spawnCountdown = AMMO_SPAWN_DELAY;
 		}
 	}
 
-	IEnumerator spawnAmmoBox(float time) {
-		yield return new WaitForSeconds(time);
-
+	private void spawnAmmoBox() {
 		// Spawn the ammo box
 		box = Instantiate(AmmoBoxPrefab);
-		box.transform.position = spawnLocations[Random.Range(0, 3)].position;
+		box.transform.position = spawnLocations[Random.Range(0, spawnLocations.Length - 1)].position;
 	}
 
 	public void pickupAmmoBox() {
 		Destroy(box);
-		ammoBoxOnMap = false;
 	}
 }
