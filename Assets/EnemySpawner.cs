b474f6e5b81502b796a6185c9ec086351c4f761e
jacwkang <jacwkang@gmail.com>
2016-03-18 01:20:12 -0700

    Updated wave system and fixed AI a bit

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -1,6 +1,6 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine.UI;
+using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
 	public Transform[] points;
@@ -13,10 +13,6 @@ public class EnemySpawner : MonoBehaviour {
 	private Text scoreCount;
 	private Text waveCount;
 
-	public GameObject tablet;
-	public Text story;
-
-
 	// Use this for initialization
 	void Start () {
 		Transform scoreUI = GameObject.Find ("HUDCanvas").transform.GetChild (3);
@@ -26,43 +22,55 @@ public class EnemySpawner : MonoBehaviour {
 		waveCount.text = "1";
 		InvokeRepeating ("Spawn", 1, 10);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		txt ();
+
 	}
 
 	void Spawn () {
 		int index = isOriginal ? 0 : 1;
-			
+
 		// Only spawn 3 waves
 		if (waveNumber <= 4) {
 			GameObject spider = Instantiate (enemy, points [index].position, points [index].rotation) as GameObject;
-
 			EnemyAI enemyAI = spider.GetComponent<EnemyAI> ();
-			Debug.Log (enemyAI);
 
 			if (isOriginal) { // Assign this the original way points
 				isOriginal = false;
 				enemyAI.patrolWayPoints = GameObject.Find("wayPoints").GetComponentsInChildren<Transform>();
-				
-			} else { // Assign this the new way points
+
+			}  else { // Assign this the new way points
 				isOriginal = true;
 				enemyAI.patrolWayPoints = GameObject.Find("wayPoints2").GetComponentsInChildren<Transform>();
 			}
 
 			numEnemies++;
 			spawnedSoFar++;
-		} else {
+		}  else {
 			// All waves are done
 			Debug.Log ("Waves are spawned");
-		}
+			if (waveNumber == 5) { // Spawn the boss
+				GameObject spider = Instantiate (enemy, points [index].position, points [index].rotation) as GameObject;
+				MeshRenderer renderer = spider.GetComponent<MeshRenderer> ();
+				renderer.material =	new Material(Shader.Find("Diffuse"));
+				renderer.material.color = new Color (0.7f, 0.8f, 0.6f);
+				spider.transform.localScale = new Vector3 (0.5f, 0.5f, 0.5f);
+				EnemyAI enemyAI = spider.GetComponent<EnemyAI> ();
+				enemyAI.radiusRange = 3f;
+				BulletInteraction bulletInteraction = spider.GetComponent<BulletInteraction> ();
+				bulletInteraction.health = 50;
 
+			}
+		}
+		if (waveNumber <= 4) {
+			waveCount.text = (waveNumber + 1).ToString ();
+		}
 		// Once 4 enemies are spawned, it's a new wave
 		if (spawnedSoFar % 4 == 0) {
-			waveNumber++;
-			waveCount.text = waveNumber.ToString ();
-			tablet.GetComponent<MapScript> ().setPrompt (true);
+			if (waveNumber <= 4) {
+				waveNumber++;
+			}
 		}
 
 	}
@@ -78,53 +86,5 @@ public class EnemySpawner : MonoBehaviour {
 		}
 
 		Debug.Log (numEnemies);
-	}
-
-	public void txt(){
-
-		//tablet.inProgress = true;
-		//tablet.showingPro = true;
-		switch (waveNumber) {
-
-		case 1:
-			story.text = "Why did the nurse leave me a gun next to my bed? Where am I? What happened to me? " +
-				"What happened here? Is my wife ok? Vanessa? Jamie? I miss my little princesses.\n(Press 'p' to close)";
-			break;
-		case 2:
-			story.text = "Oww my legs. \nOhhh yeah... A car hit me. Last I saw of my darling Audrey was next to my stretcher. " +
-				"But where is she now? Where is everyone? How long have I been here? I need to find my daughters. And whats with all these hostile spiders? \n(Press 'p' to close)";
-			break; 
-		case 3:
-			story.text = "I remember hearing a nurse playing with them. Hopefully she's taking care of them. " +
-				"I hope none of them are hurt. I need to find them now!\n(Press 'p' to close)";
-			break;
-		case 4:
-			story.color = Color.white;
-			story.fontSize = 16;
-			story.text = "From: St. Capoli Emergency Notification\nTo:undisclosed-recipients" +
-				"\n\nPossible virus outbreak seen at approx. 0.19:42 hours near 14th floor at rm. 1408." +
-				"\nLast seen spreading to the 8th floor and 16th floor." +
-				"\nRefer to HPD website for precautions to take.(Press 'p' to close)";
-			break;
-		case 5:
-			story.color = Color.green;
-			story.fontSize = 20;
-			story.text = "Arg. I can barely bend my leg. That driver looked a bit strange before he hit me.\n(Press 'p' to close)";
-			break;
-		case 6:
-			story.text = "This place feels like a maze, I feel like im walking in circles. " +
-				"How am i going to find my family. I pray that they are ok.\n(Press 'p' to close)";
-			break;
-		case 7:
-			story.text = "Maybe there is a secret exit somewhere.\n(Press 'p' to close)";
-			break;
-		case 9:
-			story.text = "This is impossible, my legs are about to give out. I can't stand this pain anymore!\n(Press 'p' to close)";
-			break;
-
-		default:
-			story.text = "Story time...\n(Press 'p' to close)";
-			break;
-		}
 	}
 }
--- Assets/Scripts/Spider/EnemyAI.cs
+++ Assets/Scripts/Spider/EnemyAI.cs
@@ -5,6 +5,8 @@ using System.Collections;
 public class EnemyAI : MonoBehaviour
 {
 	public Transform[] patrolWayPoints; // The list of patrol points the spider will cycle through
+	public int attackDamage = 50;
+	public float radiusRange;
 	private GameObject player; // The player GameObject
 	private SphereCollider spiderCollider; // The sphere collider attached to the spider
 	private NavMeshAgent nav; // Used to navigate the spider                                                           
@@ -13,10 +15,8 @@ public class EnemyAI : MonoBehaviour
 	private bool isAttacking; 
 
 	private float minRange;
-	private float attackInterval; // How long in between each attack interval
 	private float patrolSpeed = 1f; // How fast the spider will move while patrolling        
-	private float engageSpeed = 3f; // How fast the spider will engage
-	private float patrolWaitTime = 1f; // How long each spider will wait at the patrol point before moving on
+	private float engageSpeed = 4f; // How fast the spider will engage
 	private float rotationSpeed = 1f; // How fast it takes to rotate to face the player
 
 	void Awake ()
@@ -24,10 +24,11 @@ public class EnemyAI : MonoBehaviour
 		nav = GetComponent<NavMeshAgent> ();
 		player = GameObject.Find ("Player");
 		spiderCollider = GetComponent<SphereCollider> ();
-		minRange = spiderCollider.radius - 2;
+		minRange = spiderCollider.radius;
+		radiusRange = 2f;
 		//patrolWayPoints = (Transform[])GameObject.Find("wayPoints").GetComponents<Transform>();
 	}
-		
+
 	void Update ()
 	{
 		if (InSight ())
@@ -39,13 +40,12 @@ public class EnemyAI : MonoBehaviour
 	bool InSight () {
 		RaycastHit hit;
 		var rayDirection = player.transform.position - transform.position;
-		Debug.Log ("SPIDER COLLIDER RADIUS" + spiderCollider.radius);
+
 		if (Physics.Raycast(transform.position + transform.up, rayDirection.normalized, out hit, spiderCollider.radius * 2f)) {
 			if (hit.collider.gameObject == player) {
 				GetComponentInChildren<Animator> ().SetBool ("PlayerInSight", true); // Animate
-				Debug.Log("player in sight true");
 				return true;
-			} else {
+			}  else {
 				GetComponentInChildren<Animator> ().SetBool ("PlayerInSight", false);
 			}
 		}
@@ -56,7 +56,7 @@ public class EnemyAI : MonoBehaviour
 		float distance = Vector3.Distance(transform.position, player.transform.position);
 
 		// Moving towards player
-		if (distance > minRange) {
+		if (distance > radiusRange) {
 			nav.destination = player.transform.position;
 			nav.speed = engageSpeed;
 
@@ -64,13 +64,11 @@ public class EnemyAI : MonoBehaviour
 			transform.rotation = Quaternion.Slerp (transform.rotation,
 				Quaternion.LookRotation (player.transform.position - transform.position), rotationSpeed * Time.deltaTime);
 
-			// Reset attack interval time
-			attackInterval = 0;
-			
 			GetComponentInChildren<Animator> ().SetBool ("NextToPlayer", false);
-		} 
+		}  
 		// Next to player
-		else if (distance < 3f) {
+		else if (distance <= radiusRange) {
+
 			// Rotate towards the player
 			transform.rotation = Quaternion.Slerp (transform.rotation,
 				Quaternion.LookRotation (player.transform.position - transform.position), rotationSpeed * Time.deltaTime);
@@ -87,7 +85,7 @@ public class EnemyAI : MonoBehaviour
 			GetComponentInChildren<Animator>().SetBool("NextToPlayer", true);
 
 			Health health = player.GetComponent<Health>();
-			health.decreaseHealth(50);
+			health.decreaseHealth(attackDamage);
 
 			StartCoroutine ("DamageCooldown", 1.5f);
 		}
@@ -118,7 +116,9 @@ public class EnemyAI : MonoBehaviour
 			patrolTimer += Time.deltaTime;
 		}
 
-		Debug.Log ("Patrolling");
 		nav.destination = patrolWayPoints[index + 1].position;
 	}
 }
+
+
+
